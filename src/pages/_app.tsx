ca0b87e831abc0e2b49d18a696dcb8db65eb0fc3
softgenai <admin@softgen.ai>
2026-01-10 11:37:29 +0000

    fix(theme): Add ThemeProvider wrapper to _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,13 @@
 import { Toaster } from "@/components/ui/toaster";
+import { ThemeProvider } from "@/contexts/ThemeProvider";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <ThemeProvider>
       <Component {...pageProps} />
       <Toaster />
-    </>
+    </ThemeProvider>
   );
 }
